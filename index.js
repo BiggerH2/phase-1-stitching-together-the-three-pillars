8f73e82ee07bee5fbfaca1b4792fe30243dcc353
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-04-14 23:33:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      alert("You notified the server!");
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+    });
+}
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+function mimicServerCall() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve("Pretend remote server notified of action!");
+    }, 300);
+  });
+}
